6025726473a94a79a7ae584ee927b197b0b62c07
Paul Colomiets <paul@colomiets.name>
2017-03-21 19:32:35 +0200

    Always use default interval

--- src/locked.rs
+++ src/locked.rs
@@ -8,31 +8,25 @@ use tokio_core::reactor::{Handle, Interval};
 /// A wrapper around original ``self_meter::Meter`` that locks internal
 /// mutex on most operations and maybe used in multiple threads safely.
 #[derive(Clone)]
-pub struct Meter(Arc<Inner>);
+pub struct Meter(Arc<Mutex<self_meter::Meter>>);
 
-struct Inner {
-    meter: Mutex<self_meter::Meter>,
-    interval: Duration, // TODO(tailhook) this should be fetched from meter
-                        //                itself and removed from here
-}
 
 impl Meter {
+
     /// Create a new meter with specified scan interval
-    pub fn new(scan_interval: Duration) -> Meter {
-        let inner = self_meter::Meter::new(scan_interval)
+    pub fn new() -> Meter {
+        let inner = self_meter::Meter::new(Duration::new(1, 0))
             .expect("self-meter should be created successfully");
-        Meter(Arc::new(Inner {
-            meter: Mutex::new(inner),
-            interval: scan_interval,
-        }))
+        Meter(Arc::new(Mutex::new(inner)))
     }
+
     /// Adds a scanner coroutine to tokio main loop
     ///
     /// This must be called once per process (not per thread or tokio loop)
     pub fn spawn_scanner(&self, handle: &Handle) {
         let meter = self.clone();
         handle.spawn(
-            Interval::new(self.0.interval, handle)
+            Interval::new(Duration::new(1, 0), handle)
             .expect("interval should work")
             .map_err(|e| panic!("interval error: {}", e))
             .map(move |()| {
@@ -44,6 +38,6 @@ impl Meter {
     }
 
     fn lock(&self) -> MutexGuard<self_meter::Meter> {
-        self.0.meter.lock().expect("meter not poisoned")
+        self.0.lock().expect("meter not poisoned")
     }
 }
